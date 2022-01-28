8bd5ae4835617879f5919bfc1a2c5414e25ec0da
cjsmocjsmo <porthose.cjsmo.cjsmo@gmail.com>
2022-01-27 22:45:41 -0800

    extract cities

--- masonmaillists.py
+++ masonmaillists.py
@@ -10,55 +10,41 @@ mcursor.execute("CREATE TABLE mason(type TEXT, catagory TEXT, str_num TEXT, pref
 
 class MasonMailLists:
     def __init__(self):
-        pass
+        self.cities = []
 
     def split_type(self, astr):
         foo = astr.split("-")
         if len(foo) != 3:
-            pass
-        else:
             print(foo)
+        else:
             type = foo[1]
             catagory = foo[2]
             return type, catagory
 
     def split_addr(self, addr):
         addr_split = addr.split(",")
-        print(addr_split)
         adr = addr_split[0].split(" ")
-        print(adr)
-        # try:
         str_num = adr[0]
         prefix = adr[1]
         str_name = adr[2]
         suffix = adr[3]
-        # except IndexError:
-        #     print("index error {}".format(addr_split))
-
-        # try:
         cz = addr_split[1].split(" ")
-        print(cz)
         city = cz[1]
+        self.cities.append(cz[1])
         zipcode = cz[2]
-            
-        # except IndexError:
-            # print(addr_split)
         return str_num, prefix, str_name, suffix, city, zipcode
 
     def parse_mason(self):
-
         with open('/home/2021-RP-master.csv', 'r') as csv_file:
             reader = csv.reader(csv_file)
             count = 0
-
             try:
                 for row in reader:
                     count += 1
                     try:
                         type, catagory = self.split_type(row[25])
                     except TypeError:
-                        print(row[25])
-                    
+                        print(row)
                     try:
                         str_num, prefix, str_name, suffix, city, zipcode = self.split_addr(row[37])
                         entries = (type, catagory, str_num, prefix, str_name, suffix, city, zipcode)
@@ -66,10 +52,13 @@ class MasonMailLists:
                         print(mcursor.lastrowid)
                         mconn.commit()
                     except IndexError:
-                        print(row[37])
+                        print(row)
                     
-                    bar = "item" + str(count) + ":" + row[25] + "  " + row[37]
-                    print(bar)
+                    # bar = "item" + str(count) + ":" + row[25] + "  " + row[37]
+                    print(str(count))
+                    
             except UnicodeDecodeError:
                 print(row)
+
+            print(list(set(self.cities)))
         return count
